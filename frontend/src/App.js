2ac4f56664afd2faab908b22e4098ec3507eb68b
Saman Kumara <samankumararajapaksha660@gmail.com>
2024-05-10 16:31:55 +0530

    add student part frontend complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter , Routes, Route} from 'react-router-dom';
 import Student from './Student';
+import CreateStudent from './CreateStudent';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/' element= {<Student />}></Route>
+          <Route path='/create' element= {<CreateStudent />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- frontend/src/Student.js
+++ frontend/src/Student.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import {Link} from 'react-router-dom';
 
 function Student() {
 
     const [Student, setStudent] = useState([])
     useEffect(() => {
-     axios.get('https://localhost:8081/')
+     axios.get('http://localhost:8081/')
      .then(res => setStudent(res.data))
      .catch(err => console.log(err));
     }, [])
@@ -14,12 +15,13 @@ function Student() {
   return (
     <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
        <div className='w-50 bg-white rounded p-3'>
-           <button className='btn btn-success'>Add +</button>
+           <Link to="/create" className='btn btn-success'>Add +</Link>
            <table className='table'>
               <thead>
                 <tr>
                     <td>Name</td>
                     <td>Email</td>
+                    <td>Action</td>
                 </tr>
               </thead>
 
@@ -29,6 +31,10 @@ function Student() {
                         <tr key = {i}>
                            <td>{data.Name}</td>
                            <td>{data.Email}</td>
+                           <td>
+                             <button className='btn btn-primary'>Update</button> &nbsp;
+                             <button className='btn btn-danger'>Delete</button>
+                           </td>
                         </tr>
                     ) )
                   }
